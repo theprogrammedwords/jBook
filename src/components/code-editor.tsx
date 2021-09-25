93f6a7bc736d3a371565922be09def7a9616b1bd
Ashish Prasad <rprasad43255@gmail.com>
2021-09-25 18:22:02 +0530

    Added Monaco Editor

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,7 @@
+import MonacoEditor from '@monaco-editor/react'
+
+const CodeEditor = () => {
+  return <MonacoEditor language="javascript" height="250px"/>
+}
+
+export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { useState, useEffect, useRef} from "react";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = ()  => {
   const ref = useRef<any>();
@@ -69,6 +70,7 @@ const App = ()  => {
   `
   return(
     <div>
+    <CodeEditor/>
     <textarea value={input} onChange={ e => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
