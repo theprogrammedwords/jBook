3b5ac1d82fc1648757f17a2706e8e0b3c52cecbf
Ashish Prasad <rprasad43255@gmail.com>
2021-09-25 23:29:44 +0530

    Added syntax highlighting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,85 @@
-import MonacoEditor from '@monaco-editor/react'
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useRef } from 'react';
+import { getTokenSourceMapRange } from 'typescript';
+import './code-editor.css';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
+import './syntax.css';
 
-const CodeEditor = () => {
-  return <MonacoEditor language="javascript" height="250px"/>
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
 }
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+    const highlighter = new Highlighter(
+      //@ts-ignore
+      window.monaco,
+      codeShift,
+      monacoEditor
+    );
+
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
+  };
+
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+    const formatted = prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
+
+    editorRef.current.setValue(formatted);
+  };
+
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={onEditorDidMount}
+        theme="dark"
+        language="javascript"
+        height="250px"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 14,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
 
 export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,50 @@
 import * as esbuild from 'esbuild-wasm';
-import ReactDOM from "react-dom";
-import { useState, useEffect, useRef} from "react";
+import ReactDOM from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
-const App = ()  => {
+const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
-  const [input, setInput] = useState("")
-  const [code, setCode] = useState("")
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-  const startService = async() => {
+  const startService = async () => {
     ref.current = await esbuild.startService({
-      worker : true,
-      wasmURL : 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    })
-  }
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
 
-  useEffect(()=> {
+  useEffect(() => {
     startService();
-  })
+  });
 
   const onClick = async () => {
-    if(!ref.current){
-      return
+    if (!ref.current) {
+      return;
     }
 
     iframe.current.src = html;
 
     const result = await ref.current.build({
-      entryPoints : ['index.js'],
-      bundle : true,
-      write : false,
-      plugins : [unpkgPathPlugin(), fetchPlugin(input)],
-      define : {
-        'process.env.NODE_ENV' : '"production"',
-         global : 'window'
-      }
-    })
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-
-    //console.log(result)  
+    //console.log(result)
     //setCode(result.outputFiles[0].text)
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
-  }
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
 
   const html = `
     <html>
@@ -67,23 +67,24 @@ const App = ()  => {
         </script>
       </body>
     </html>
-  `
-  return(
+  `;
+  return (
     <div>
-    <CodeEditor/>
-    <textarea value={input} onChange={ e => setInput(e.target.value)}></textarea>
+      <CodeEditor
+        initialValue="//enter your code here"
+        onChange={(value) => setInput(value)}
+      />
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe ref={iframe} srcDoc={html} sandbox="allow-scripts"/>
-    </div>   
-  )
+      <iframe ref={iframe} srcDoc={html} sandbox="allow-scripts" />
+    </div>
+  );
+};
 
-}
-
-ReactDOM.render(
-  <App/>,
-  document.querySelector('#root')
-) 
-
+ReactDOM.render(<App />, document.querySelector('#root'));
